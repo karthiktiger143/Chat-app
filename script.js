e8d9bcfcab5cf284ab5a737508ca65a227383da4
karthiktiger143 <kartbar276@gmail.com>
2026-05-17 18:44:19 +0530

    script.js

--- script.js
+++ script.js
@@ -1,68 +1,39 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
 
 import {
-getAuth,
-RecaptchaVerifier,
-signInWithPhoneNumber
-
+  getAuth,
+  RecaptchaVerifier,
+  signInWithPhoneNumber
 } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
 
 const firebaseConfig = {
-
-apiKey: "AIzaSyCM2CUrWEyad180qCy-34PC4A0HOLaSWsg",
-
-authDomain: "nammaapp-e8b13.firebaseapp.com",
-
-projectId: "nammaapp-e8b13",
-
-storageBucket: "nammaapp-e8b13.firebasestorage.app",
-
-messagingSenderId: "1068448599469",
-
-appId: "1:1068448599469:web:8f02d17e456597de75e524",
-
-measurementId: "G-ZGL72NLPVS"
-
+  apiKey: "AIzaSyCM2CUrWEyad180qCy-34PC4A0HOLaSWsg",
+  authDomain: "nammaapp-e8b13.firebaseapp.com",
+  projectId: "nammaapp-e8b13",
+  storageBucket: "nammaapp-e8b13.firebasestorage.app",
+  messagingSenderId: "1068448599469",
+  appId: "1:1068448599469:web:8f02d17e456597de75e524"
 };
 
 const app = initializeApp(firebaseConfig);
-
 const auth = getAuth(app);
 
-window.recaptchaVerifier = new RecaptchaVerifier(
-auth,
-'recaptcha-container',
-{}
-);
+window.recaptchaVerifier = new RecaptchaVerifier(auth, 'sendOtp', {
+  'size': 'normal'
+});
 
-window.sendOTP = async function(){
+const sendOtpBtn = document.getElementById("sendOtp");
 
-const phoneNumber =
-document.getElementById('phone').value;
+sendOtpBtn.addEventListener("click", () => {
+  const number = "+91" + document.getElementById("phone").value;
 
-const appVerifier =
-window.recaptchaVerifier;
-
-confirmationResult =
-await signInWithPhoneNumber(
-auth,
-phoneNumber,
-appVerifier
-);
-
-alert("OTP Sent");
-
-}
-
-window.verifyOTP = async function(){
-
-const code =
-document.getElementById('otp').value;
-
-await confirmationResult.confirm(code);
-
-alert("Login Success");
-
-window.location.href = "home.html";
-
-}
+  signInWithPhoneNumber(auth, number, window.recaptchaVerifier)
+    .then((confirmationResult) => {
+      window.confirmationResult = confirmationResult;
+      alert("OTP Sent");
+    })
+    .catch((error) => {
+      alert(error.message);
+      console.log(error);
+    });
+});
